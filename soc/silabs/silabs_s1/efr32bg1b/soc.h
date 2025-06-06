8d6e3cefe04f09ee3037feddf670ab4fce1a2427
Orange-Murker <23561952+Orange-Murker@users.noreply.github.com>
2025-06-06 09:42:47 +0200

    feat: add support for efr32bg1 soc

---
+++ soc/silabs/silabs_s1/efr32bg1b/soc.h
@@ -0,0 +1,16 @@
+#ifndef EFR32BG1B_SOC_H_
+#define EFR32BG1B_SOC_H_
+
+#include <zephyr/sys/util.h>
+
+#ifndef _ASMLANGUAGE
+
+#include <em_common.h>
+
+#include "soc_pinmap.h"
+#include "../common/soc_gpio.h"
+
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* EFR32BG1B_SOC_H_ */
